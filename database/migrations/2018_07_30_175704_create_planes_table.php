f6ddc5da3171c623e1748062ca2f824a37d04d40
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2018-07-31 18:00:19 -0300

    Módulo 2 Aula 02 - Migrations (Parte 01)

--- database/migrations/2018_07_30_175704_create_planes_table.php
+++ database/migrations/2018_07_30_175704_create_planes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePlanesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('planes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('image', 200);
-            $table->rememberToken();
+            $table->integer('brand_id')->unsigned();
+            $table->integer('qty_passangers');
+            $table->enum('class', ['economic', 'luxury']);
             $table->timestamps();
+
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('planes');
     }
 }
--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@
 
 **Feito**
 **Modulo**: 2
-**Aula**: 1
+**Aula**: 2
 
 ---
 
@@ -17,10 +17,28 @@
 
 ```
 
+php artisan make:model Models\\Brand -m
+
+php artisan make:model Models\\Plane -m
+
 
 
 ```
 
 
 ## Anotações
+
+
+Foi incluído no provider AppServiceProvider a linha 
+Schema::defaultStringLength(191);
+
+Foram incluídas as linhas abaixo na configuração do MySQL
+'modes'  => [
+                'ONLY_FULL_GROUP_BY',
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_ENGINE_SUBSTITUTION',
+            ],
 
